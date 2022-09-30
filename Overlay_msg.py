e6c8644844e79d564777d150b9f5da67fbf59522
NappingSodion <53437291+KJH-x@users.noreply.github.com>
2022-09-30 12:03:53 +0800

    Update Overlay_msg.py

--- Overlay_msg.py
+++ Overlay_msg.py
@@ -1,111 +1,173 @@
 # encoding=utf-8
 '''
 Filename :overlay text.py
-Description :N/A
-Datatime :2022/09/18
+Description :...
+Datatime :2022/09/30
 Author :KJH
-Version :v1.0
+Version :v2.0
 '''
 import pyperclip
-import ctypes
+from ctypes import windll
 import win32api
+import win32gui
+import win32con
 import tkinter
+from random import randint
+from tkinter import Event
 import os
 import sys
-os.system("chcp 65001")
-os.chdir(sys.path[0])
 
 
-def MouseDown(event):
+def mouse_left_click(event: Event):
+    """
+    Get Mouse position when left mouse button pressed
+    """
     global mousX, mousY
+    print(f"mouse_left_click:\n{event}")
 
     mousX = event.x
     mousY = event.y
 
 
-def MouseMoveW1(event):
+def mouse_drag(event: Event):
+    """
+    Move the frame when left mouse button pressed and dragged
+    """
     global mw, mousX, mousY, txt
+    print(f"mouse_drag:\n{event}")
 
     osx = mousX+txt.winfo_x()
     osy = mousY+txt.winfo_y()
     mw.geometry(f'+{event.x_root - osx}+{event.y_root - osy}')
 
 
-def quit_command(event):
+def quit_command(event: Event):
+    """
+    Destroy and exit
+    """
     print("quit")
     exit()
 
 
-def resize(event):
+def resize_label(event: Event):
+    """
+    Enlarge when mouse wheel up or up arrow key pressed
+    Shrink when mouse wheel down or down arrow key pressed
+    """
     global mw, textsize, txt
-    print(f"wheel:\n{event}")
+    print(f"resize_label:\n{event}")
+
     if event.delta > 0 or event.keysym == "Up":
         textsize += int(0.2*textsize)
     elif event.delta < 0 or event.keysym == "Down":
         textsize -= int(0.2*textsize)
-
     txt.configure(font=("华文中宋", textsize))
-
     update_posit(event)
 
 
-def update_content(event):
-    global mw, txt
-    print(f"Update content!\n{event}\n")
-    txt.configure(text=str(pyperclip.paste()))
+def change_content(event: Event):
+    """
+    Replace content when Ctrl+R pressed
+    Append content to the end with return when Ctrl+Shift+R pressed
+    Append content to the head with return when Ctrl+Shift+Alt+R pressed
+    """
+    global mw, txt, display_content
+    print(f"change_content:\n{event}")
 
+    if event.state == 12:
+        # aka. ctrl+
+        display_content = f"{pyperclip.paste()}"
+    elif event.state == 13:
+        # aka. ctrl+shift+
+        display_content = f"{display_content}\n{pyperclip.paste()}"
+    elif event.state == 131085:
+        # aka. ctrl+shift+alt
+        display_content = f"{pyperclip.paste()}\n{display_content}"
+    txt.configure(text=display_content)
     update_posit(event)
+    return
 
 
-def update_posit(event):
+def update_posit(event: Event):
+    """
+    Refresh size and position when content or size change
+    """
     global mw, txt
+    print(f"---update_posit")
+
     (CursorX, CursorY) = win32api.GetCursorPos()
     mw.geometry(f'+{abs(event.x - CursorX)}+{abs(event.y - CursorY)}')
-    print(f"position set to {abs(event.x - CursorX)},{abs(event.y - CursorY)}")
+    print(
+        f"---position set to {abs(event.x - CursorX)},{abs(event.y - CursorY)}"
+    )
 
 
-def alpha_change(event):
+def change_alpha(event: Event):
+    """
+    Change opacity when Ctrl"""
     global mw, txt, alpha
-    print("control+wheel", event)
+    print(f"change_alpha:\n{event}")
+
     if event.delta > 0 or event.keysym == "Up":
         alpha += 0.05
     elif event.delta < 0 or event.keysym == "Down":
         alpha -= 0.05
-
     mw.attributes('-alpha', alpha)
 
 
-textsize = 50
-alpha = 0.5
-display_content = str(pyperclip.paste())
-mw = tkinter.Tk()
-ctypes.windll.shcore.SetProcessDpiAwareness(1)
-font_family1 = ("华文中宋", textsize)
-
-mw.overrideredirect(1)
-mw.attributes('-alpha', alpha)
-mw.attributes("-topmost", 1)
-mw.bind("<Escape>", quit_command)
-mw.bind('<Control-c>', quit_command)
-mw.bind('<Control-C>', quit_command)
-mw.bind('<MouseWheel>', alpha_change)
-mw.bind('<Up>', alpha_change)
-mw.bind('<Down>', alpha_change)
-
-mw.bind('<Control-MouseWheel>', resize)
-mw.bind('<Control-Up>', resize)
-mw.bind('<Control-Down>', resize)
-
-mw.bind('<Control-r>', update_content)
-mw.bind('<Control-R>', update_content)
-
-txt = tkinter.Label(mw, text=display_content, font=font_family1,
-                    bg="black", fg="white")
-txt.pack()
-txt.bind("<Button-1>", MouseDown)
-txt.bind("<B1-Motion>", MouseMoveW1)
+def event_test(event: Event):
+    print(f"{event}")
+    return
 
 
-(CursorX, CursorY) = win32api.GetCursorPos()
-mw.geometry(f"+{CursorX}+{CursorY}")
-mw.mainloop()
+if __name__ == "__main__":
+
+    os.system("chcp 65001")
+    os.chdir(sys.path[0])
+    title = f"[{str(pow(randint(1,10),randint(10,20)))}]"
+    os.system(f"title {title}")
+
+    textsize = 50
+    alpha = 0.5
+    display_content = pyperclip.paste()
+    mw = tkinter.Tk()
+    windll.shcore.SetProcessDpiAwareness(1)
+    font_family1 = ("华文中宋", textsize)
+
+    debug = 0
+    if not debug:
+        print(win32gui.ShowWindow(
+            win32gui.FindWindow(0, title),
+            win32con.HIDE_WINDOW
+        ))
+
+    mw.overrideredirect(1)
+    mw.attributes('-alpha', alpha)
+    mw.attributes("-topmost", 1)
+    mw.bind("<Escape>", quit_command)
+    mw.bind('<Control-c>', quit_command)
+    mw.bind('<Control-C>', quit_command)
+    mw.bind('<MouseWheel>', change_alpha)
+    mw.bind('<Up>', change_alpha)
+    mw.bind('<Down>', change_alpha)
+
+    mw.bind('<Control-MouseWheel>', resize_label)
+    mw.bind('<Control-Up>', resize_label)
+    mw.bind('<Control-Down>', resize_label)
+
+    mw.bind('<Control-r>', change_content)
+    mw.bind('<Control-R>', change_content)
+    mw.bind('<Control-Shift-r>', change_content)
+    mw.bind('<Control-Shift-R>', change_content)
+    mw.bind('<Control-Shift-Alt-r>', change_content)
+    mw.bind('<Control-Shift-Alt-R>', change_content)
+
+    txt = tkinter.Label(mw, text=display_content, font=font_family1,
+                        bg="black", fg="white")
+    txt.pack()
+    txt.bind("<Button-1>", mouse_left_click)
+    txt.bind("<B1-Motion>", mouse_drag)
+
+    (CursorX, CursorY) = win32api.GetCursorPos()
+    mw.geometry(f"+{CursorX}+{CursorY}")
+    mw.mainloop()
